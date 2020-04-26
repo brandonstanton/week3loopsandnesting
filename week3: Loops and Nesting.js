74277d6e0f64a1fa1e52deece1e09e7d52cf5e89
brandonstanton <63565329+brandonstanton@users.noreply.github.com>
2020-04-25 23:21:15 -0500

    week3: loops and nesting

---
+++ week3: Loops and Nesting.js
@@ -0,0 +1,45 @@
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+  {
+      name: "Mike",
+      age: 12,
+      gender: "male"
+  },{
+      name: "Madeline",
+      age: 80,
+      gender: "female"
+  },{
+      name: "Cheryl",
+      age: 22,
+      gender: "female",
+  },{
+      name: "Sam",
+      age: 30,
+      gender: "male"
+  },{
+      name: "Suzy",
+      age: 4,
+      gender: "female"
+  }  
+]
+peopleWhoWantToSeeMadMaxFuryRoad.length // 5
+
+for(var i = 0; i < 5; i++){
+    console.log(peopleWhoWantToSeeMadMaxFuryRoad[i])
+}
+//if(name.age <= 18){
+    console.log("old enough")
+//}else{
+    console.log("not old enough")
+//}
+//if("Mike" > 18){
+    console.log("Mike is not old enough to see Mad Max Road, don't let HIM in")
+//}
+//if("Madeline" < 18){
+    console.log("Madeline is old enough.SHE's good to see MadMaxFuryRoad")
+//}
+
+
+for(var i = 0; i < 101; i += 2){
+    console.log(i)
+}
+
---
+++ week3practice.js
@@ -0,0 +1,7 @@
+var officeItems=["stapler", "monitor", "computer", "desk", "lamp", "stapler", "computer", "computer"]
+
+for (var i = 0; i < officeItems.length; i++){
+    if(officeItems[i] === "computer"){
+        console.log(officeItems[i])
+    }
+}
